11e8a77c6cb17bc58f3fc1476e3e8bedd5686f73
kncode86 <kulcsar.norbert.86@gmail.com>
2024-11-14 16:35:40 +0000

    feat: Create Dashboard

--- resources/js/Pages/Task/Show.jsx
+++ resources/js/Pages/Task/Show.jsx
@@ -7,10 +7,19 @@ export default function Show({ auth, task }) {
         <AuthenticatedLayout
             user={auth.user}
             header={
-                <h2 className="font-semibold text-xl text-gray-800
-                dark:text-gray-200 leading-tight">
-                    {`Task "${task.name}"`}
-                </h2>
+                <div className="flex items-center justify-between">
+                    <h2 className="font-semibold text-xl text-gray-800
+                    dark:text-gray-200 leading-tight">
+                        {`Task "${task.name}"`}
+                    </h2>
+                    <Link
+                        href={route('task.edit', task.id)} 
+                        className="bg-emerald-500 px-3 py-1 text-white rounded shadow transition-all hover:bg-emerald-600"
+                    >
+                        Edit
+                    </Link>
+                </div>
+               
             }
         >
             <Head title={`Task ${task.name}`} />
